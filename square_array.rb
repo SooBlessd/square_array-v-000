3c282594094f30fe9f7c6374400f732ce6b09dd6
Valencia Caldwell <vkcaldwell@gmail.com>
2021-01-09 00:11:57 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,19 @@
-def square_array(array)
-  # your code here
+number=[1,2,3]
+def square_array(number)
+  number.each do |i| 
+      i ** 2
+  end
+  print square_array(number)
 end
+# def new_numbers
+# new_numbers=[square_array(array)]
+  
+
+    
+# #     # your code here
+# # end
+# # square_array
+# def square_array(array)
+#   array.each { |x| x ** 2 } == new_array
+#   print new_array
+# end
